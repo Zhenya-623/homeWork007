7da295b2dc3906d2115587b57f05499da330e008
Евгений Политыко <Zhenya-623@internet.ru>
2022-12-03 12:42:40 +0300

    task50 is ready

---
+++ task50/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1,7 -> такого элемента в массиве нет
+
+int[,] InitMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+int[,] array2D = InitMatrix(5, 5, -10, 10);
+PrintMatrix(array2D);
+
+Console.Write("Введите номер строки искомого элемента: ");
+int rowsFind = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца искомого элемента: ");
+int columnsFind = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Значение искомого элемента равно -> {array2D[rowsFind - 1, columnsFind - 1]}");
