92fcc20ce66d8eed31dd818e97a0c574d5aa8e91
Евгений Политыко <Zhenya-623@internet.ru>
2022-12-05 10:12:16 +0300

    homeWork is ready

---
+++ task52/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] InitMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+double[] AveragesColumns(int[,] matrix)
+{
+    int y = 0;
+    double[] array = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        int count = matrix.GetLength(0);
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+            array[y] = sum / count;
+        }
+        y++;
+    }
+    return array;
+}
+
+
+void PrintArray(double[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{Math.Round(array[i],1)},   ");
+        else Console.Write($"{Math.Round(array[i],1)}");
+    }
+    Console.WriteLine(" ]");
+}
+
+
+int[,] array2D = InitMatrix(4, 4, 1, 10);
+PrintMatrix(array2D);
+
+double[] resultArr = AveragesColumns(array2D);
+Console.WriteLine("Среднее арифметическое значение суммы цифр каждого столбца");
+PrintArray(resultArr);
+
